bf65b04f93eead052a8d6c72b6008466bc58b5fe
Magnus Görlitz <magnus.goerlitz@check24.de>
2019-09-09 22:44:21 +0200

    Publish to gh pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS(withSass());
+module.exports = {
+  ...withCSS(withSass()),
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/cv' : '',
+};
