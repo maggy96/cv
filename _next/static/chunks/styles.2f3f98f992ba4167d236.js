41959ba7eec1e44573c64eb4bb067329aab9d40e
Magnus Görlitz <magnus.goerlitz@check24.de>
2020-01-21 13:11:39 +0100

    Package updates

---
+++ _next/static/chunks/styles.2f3f98f992ba4167d236.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{MbDd:function(n,w,o){}}]);
--- _next/static/chunks/styles.fbd855bc2f621fbba9f5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{MbDd:function(n,w,o){}}]);
